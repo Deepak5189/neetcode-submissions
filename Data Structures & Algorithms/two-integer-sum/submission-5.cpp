26be3bef941c7b521aa54ca99d329e642ae62823
Deepak Singh <deepaks.rawat144@gmail.com>
2026-05-30 22:30:55 +0530

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for(int i=0;i<nums.size();i++){
+            int b=target-nums[i];
+            if(mp.find(b)!=mp.end()){
+                return {mp[b], i};
+            }
+            mp[nums[i]]=i;
+        }
+        return {-1, -1};
+    }
+};
