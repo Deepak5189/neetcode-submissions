ae9256ebdb3ebd681bff022ac0a2d20a14402117
Deepak Singh <deepaks.rawat144@gmail.com>
2026-07-04 13:17:25 +0530

    Add: last-stone-weight - submission-5

---
+++ Data Structures & Algorithms/last-stone-weight/submission-5.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> max_heap;
+        for(auto& n: stones) max_heap.push(n);
+
+        while(max_heap.size()>1){
+            int s1=max_heap.top(); max_heap.pop();
+            int s2=max_heap.top(); max_heap.pop();
+            if(s1-s2 >0) max_heap.push(s1-s2);
+        }
+        return max_heap.size()==0?0:max_heap.top();
+    }
+};
