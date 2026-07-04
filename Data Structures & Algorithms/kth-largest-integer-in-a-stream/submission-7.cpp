34b5e3604c3dbfc7c63c736718f13a172450afbe
Deepak Singh <deepaks.rawat144@gmail.com>
2026-07-04 13:06:30 +0530

    Add: kth-largest-integer-in-a-stream - submission-7

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-7.cpp
@@ -0,0 +1,19 @@
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> min_heap;
+    int k;
+    KthLargest(int k, vector<int>& nums) {
+        int i=0;
+        this->k=k;
+        for(auto&n:nums){
+            min_heap.push(n);
+            if(min_heap.size()>this->k) min_heap.pop();
+        }
+    }
+    
+    int add(int val) {
+        min_heap.push(val);
+        if(min_heap.size()>this->k) min_heap.pop();
+        return min_heap.top();
+    }
+};
