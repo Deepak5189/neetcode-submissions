fad3dafc88ccd403e17d317db0fbd8518bf531d8
Deepak Singh <deepaks.rawat144@gmail.com>
2026-07-07 12:39:18 +0530

    Add: subsets - submission-5

---
+++ Data Structures & Algorithms/subsets/submission-5.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    void solve(vector<int> &nums, int i, vector<int> vec){
+        if(i==nums.size()){
+            res.push_back(vec);
+            return;
+        }
+        solve(nums, i+1, vec);
+        vec.push_back(nums[i]);
+        solve(nums, i+1, vec);
+        return;
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> vec;
+        solve(nums, 0, vec);
+        return res;
+    }
+};
